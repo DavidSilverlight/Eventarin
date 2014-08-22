57a10d35c030b22b28bb3bbf77791c8c4bfc61a5
DavidSilverlight <DavidSilverlight@gmail.com>
2014-08-22 01:21:50 -0400

    Added Updated set of files for the initial codebase

---
+++ Eventarin/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Eventarin
+{
+	public class App
+	{
+
+		public static Page GetMainPage ()
+		{
+			var md = new MasterDetailPage ();
+
+			md.Master = new MenuPage (md);
+			md.Detail = new NavigationPage(new AboutPage ());
+
+			return md;
+		}
+		/*
+		static SQLite.Net.SQLiteConnection conn;
+		static EvolveDatabase database;
+		public static void SetDatabaseConnection (SQLite.Net.SQLiteConnection connection)
+		{
+			conn = connection;
+			database = new EvolveDatabase (conn);
+		}
+		public static EvolveDatabase Database {
+			get { return database; }
+		}
+*/
+
+		public static Color NavTint {
+			get {
+				return Color.FromHex ("3498DB"); // Xamarin Blue
+			}
+		}
+		public static Color HeaderTint {
+			get {
+				return Color.FromHex ("2C3E50"); // Xamarin DarkBlue
+			}
+		}
+	}
+}
+
---
+++ Eventarin/Models/Favorite.cs~
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Eventarin
+{
+	public class Favorite
+	{
+		public Favorite ()
+		{
+		}
+
+		public int SessionId { get; set; }
+		public bool IsFavorite { get; set; }
+	}
+}
+
